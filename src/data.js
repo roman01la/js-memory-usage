6c78ef48ed86114a43b934c25addfa4072ef205a
Roman Liutikov <roman01la@romanliutikov.com>
2018-05-11 14:33:16 +0300

    update charts

--- src/data.js
+++ src/data.js
@@ -1,228 +1,228 @@
 export const dataMap = [
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     b0: 196.32,
     b1: 882.304,
     b2: 6869.504,
     b3: 60768.392
   },
   {
-    name: 'ImmutableJS',
-    b0: 23.16,
-    b1: 1901.608,
-    b2: 17411.496,
-    b3: 182600.704
+    name: "ImmutableJS",
+    b0: 14.592,
+    b1: 1813.28,
+    b2: 17392.312,
+    b3: 182587.248
   },
   {
-    name: 'ClojureScript',
-    b0: 11.368,
-    b1: 1366.72,
-    b2: 8896.968,
-    b3: 121328.16
+    name: "ClojureScript",
+    b0: 10.696,
+    b1: 980,
+    b2: 6316.848,
+    b3: 102349.784
   },
   {
-    name: 'Kotlin',
-    b0: 454.968,
-    b1: 1725.72,
-    b2: 20542.936,
-    b3: 121960.52
+    name: "Kotlin",
+    b0: 322.904,
+    b1: 1580.992,
+    b2: 14366.752,
+    b3: 121939.384
   },
   {
-    name: 'GopherJS',
+    name: "GopherJS",
     b0: 74,
     b1: 961,
     b2: 13474,
     b3: 170444
   },
   {
-    name: 'RacketScript',
+    name: "RacketScript",
     b0: 235.8,
     b1: 1645.144,
     b2: 13579.56,
     b3: 146484.752
   },
   {
-    name: 'Reason',
-    b0: 71,
-    b1: 1363,
-    b2: 12081,
-    b3: 120077
+    name: "Reason",
+    b0: 64,
+    b1: 1296,
+    b2: 12064,
+    b3: 120064
   },
   {
-    name: 'Elm',
+    name: "Elm",
     b0: 11.368,
     b1: 1076.36,
     b2: 10436.36,
     b3: 104329.2
   },
   {
-    name: 'ElixirScript',
-    b0: 199.928,
-    b1: 885.888,
-    b2: 6876.048,
-    b3: 61020.504
+    name: "ElixirScript",
+    b0: 198.816,
+    b1: 873.832,
+    b2: 6876.056,
+    b3: 61026.376
   }
 ];
 
 export const dataSet = [
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     b0: 166.64,
     b1: 431.76,
     b2: 2645.504,
     b3: 20563.264
   },
   {
-    name: 'ImmutableJS',
-    b0: 23.192,
-    b1: 1578.704,
-    b2: 15679.656,
-    b3: 123834.648
+    name: "ImmutableJS",
+    b0: 14.16,
+    b1: 1518.16,
+    b2: 15664.728,
+    b3: 123816.36
   },
   {
-    name: 'ClojureScript',
-    b0: 11.368,
-    b1: 732.528,
-    b2: 5402.552,
-    b3: 123852.92
+    name: "ClojureScript",
+    b0: 10.696,
+    b1: 538.616,
+    b2: 5744.136,
+    b3: 140320.296
   },
   {
-    name: 'Kotlin',
-    b0: 497.112,
-    b1: 1072.216,
-    b2: 5630.728,
-    b3: 50820.536
+    name: "Kotlin",
+    b0: 380.688,
+    b1: 942.296,
+    b2: 5535.496,
+    b3: 50716.304
   },
   {
-    name: 'Reason',
-    b0: 71,
-    b1: 975,
-    b2: 8082,
-    b3: 80404
+    name: "Reason",
+    b0: 64,
+    b1: 886,
+    b2: 8067,
+    b3: 80425
   },
   {
-    name: 'Elm',
+    name: "Elm",
     b0: 11.424,
     b1: 1114.608,
     b2: 10475.792,
     b3: 104248.064
   },
   {
-    name: 'ElixirScript',
-    b0: 622.424,
-    b1: 1072.96,
-    b2: 7068.576,
-    b3: 60948.184
+    name: "ElixirScript",
+    b0: 622.384,
+    b1: 1067.416,
+    b2: 7063.152,
+    b3: 60951.816
   }
 ];
 
 export const dataIdxed = [
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     b0: 93.04,
     b1: 246.152,
     b2: 1341.864,
     b3: 11597.864
   },
   {
-    name: 'ImmutableJS',
-    b0: 19.904,
-    b1: 641.752,
-    b2: 2647.36,
-    b3: 20485.224
+    name: "ImmutableJS",
+    b0: 16.216,
+    b1: 570.944,
+    b2: 2618.096,
+    b3: 20438.92
   },
   {
-    name: 'ClojureScript',
-    b0: 11.776,
-    b1: 286.744,
-    b2: 1600.8,
-    b3: 11235.368
+    name: "ClojureScript",
+    b0: 10.824,
+    b1: 255.928,
+    b2: 1576.528,
+    b3: 11224.8
   },
   {
-    name: 'Kotlin',
-    b0: 107.24,
-    b1: 262.624,
-    b2: 1166.744,
-    b3: 9695.568
+    name: "Kotlin",
+    b0: 99.144,
+    b1: 255.6,
+    b2: 1237.784,
+    b3: 10431.176
   },
   {
-    name: 'GopherJS',
+    name: "GopherJS",
     b0: 267,
     b1: 385,
     b2: 781,
     b3: 4344
   },
   {
-    name: 'RacketScript',
+    name: "RacketScript",
     b0: 138.48,
     b1: 285.048,
     b2: 1234.384,
     b3: 10428.632
   },
   {
-    name: 'Reason',
-    b0: 145,
-    b1: 207,
-    b2: 927,
-    b3: 8122
+    name: "Reason",
+    b0: 112,
+    b1: 203,
+    b2: 912,
+    b3: 8112
   },
   {
-    name: 'Elm',
+    name: "Elm",
     b0: 11.456,
     b1: 208.288,
     b2: 1267.408,
     b3: 11220.536
   },
   {
-    name: 'ElixirScript',
+    name: "ElixirScript",
     b0: 88.704,
-    b1: 546.928,
-    b2: 6290.04,
-    b3: 49294.264
+    b1: 544.272,
+    b2: 6290.136,
+    b3: 49294.2
   }
 ];
 
 export const dataList = [
   {
-    name: 'ClojureScript',
-    b0: 8.936,
-    b1: 783.92,
-    b2: 7239.256,
-    b3: 72040.64
+    name: "ClojureScript",
+    b0: 10.92,
+    b1: 749.56,
+    b2: 7215.912,
+    b3: 72015.488
   },
   {
-    name: 'Kotlin',
-    b0: 117.872,
-    b1: 252,
-    b2: 1165.448,
-    b3: 9692.632
+    name: "Kotlin",
+    b0: 101.344,
+    b1: 253.648,
+    b2: 1238.616,
+    b3: 10430.664
   },
   {
-    name: 'RacketScript',
+    name: "RacketScript",
     b0: 20.104,
     b1: 606.568,
     b2: 5637.992,
     b3: 55971
   },
   {
-    name: 'Reason',
-    b0: 84,
-    b1: 720,
-    b2: 6480,
-    b3: 64073
+    name: "Reason",
+    b0: 71,
+    b1: 707,
+    b2: 6464,
+    b3: 64064
   },
   {
-    name: 'Elm',
+    name: "Elm",
     b0: 43.776,
     b1: 524.696,
     b2: 4851.144,
     b3: 48046.928
   },
   {
-    name: 'ElixirScript',
+    name: "ElixirScript",
     b0: 47.344,
-    b1: 215.232,
-    b2: 867.008,
-    b3: 8064.04
+    b1: 215.744,
+    b2: 870.384,
+    b3: 8063.208
   }
 ];
