cec24f754369fa50035e42adf1f73644e3a751bb
Roman Liutikov <roman01la@romanliutikov.com>
2018-09-25 15:13:48 +0300

    add F#

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { h, render, Component } from 'preact';
+import { h, render, Component } from "preact";
 import {
   BarChart,
   Bar,
@@ -8,21 +8,21 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer
-} from 'recharts';
+} from "recharts";
 
-import styles from './styles';
-import labels from './labels';
-import { formatSize } from './formatters';
-import { dataMap, dataSet, dataIdxed, dataList } from './data';
-import Tabs from './tabs';
-import renderToolTip from './tooltip';
-import renderLegend from './legend';
+import styles from "./styles";
+import labels from "./labels";
+import { formatSize } from "./formatters";
+import { dataMap, dataSet, dataIdxed, dataList } from "./data";
+import Tabs from "./tabs";
+import renderToolTip from "./tooltip";
+import renderLegend from "./legend";
 
 const colors = {
-  b0: '#540D6E',
-  b1: '#EE4266',
-  b2: '#3BCEAC',
-  b3: '#0EAD69'
+  b0: "#540D6E",
+  b1: "#EE4266",
+  b2: "#3BCEAC",
+  b3: "#0EAD69"
 };
 
 const sum = ({ b0, b1, b2, b3 }) => b0 + b1 + b2 + b3;
@@ -35,10 +35,10 @@ const chartData = {
 };
 
 const hints = {
-  List: 'singly linked list',
-  Map: 'collection of key value pairs',
-  Array: 'indexed collection',
-  Set: 'collection of unique values'
+  List: "singly linked list",
+  Map: "collection of key value pairs",
+  Array: "indexed collection",
+  Set: "collection of unique values"
 };
 
 const dstypes = {
@@ -47,7 +47,9 @@ const dstypes = {
     Elm: 1,
     RacketScript: 1,
     Reason: 1,
-    ClojureScript: 1
+    ClojureScript: 1,
+    ElixirScript: 1,
+    "F# (Fable)": 1
   },
   Map: {
     JavaScript: 0,
@@ -57,7 +59,9 @@ const dstypes = {
     RacketScript: 1,
     GopherJS: 0,
     ImmutableJS: 1,
-    Kotlin: 0
+    Kotlin: 0,
+    ElixirScript: 1,
+    "F# (Fable)": 1
   },
   Array: {
     RacketScript: 0,
@@ -67,7 +71,9 @@ const dstypes = {
     Elm: 1,
     ClojureScript: 1,
     JavaScript: 0,
-    ImmutableJS: 1
+    ImmutableJS: 1,
+    ElixirScript: 1,
+    "F# (Fable)": 1
   },
   Set: {
     JavaScript: 0,
@@ -75,7 +81,9 @@ const dstypes = {
     Elm: 1,
     Kotlin: 0,
     ClojureScript: 1,
-    ImmutableJS: 1
+    ImmutableJS: 1,
+    ElixirScript: 1,
+    "F# (Fable)": 1
   }
 };
 
@@ -85,31 +93,31 @@ const getPath = (x, y, width, height) => {
 
 const shapeBar = (dkey, ds) => props => {
   const { fill, x, y, width, height, name } = props;
-  const title = dstypes[ds][name] === 0 ? 'MUTABLE' : 'IMMUTABLE';
-  const fillLine = props[dkey] !== undefined ? fill : 'transparent';
+  const title = dstypes[ds][name] === 0 ? "MUTABLE" : "IMMUTABLE";
+  const fillLine = props[dkey] !== undefined ? fill : "transparent";
   let fs = width / 10;
   fs = fs <= 10 ? 10 : fs;
   return h(
-    'g',
+    "g",
     {},
     h(
-      'text',
+      "text",
       {
         x: x + width / 2,
         y: y - fs,
         fontSize: fs,
         fontWeight: 600,
-        textAnchor: 'middle',
+        textAnchor: "middle",
         fill: fillLine
       },
       title
     ),
-    h('path', {
+    h("path", {
       d: getPath(x, y - 6, width, 2),
-      stroke: 'none',
+      stroke: "none",
       fill: fillLine
     }),
-    h('path', { d: getPath(x, y, width, height), stroke: 'none', fill })
+    h("path", { d: getPath(x, y, width, height), stroke: "none", fill })
   );
 };
 
@@ -118,7 +126,7 @@ class Chart extends Component {
     super();
     this.state = {
       hiddenKeys: new Set(),
-      currTab: 'List'
+      currTab: "List"
     };
     this._toggleDataKey = this._toggleDataKey.bind(this);
     this._selectTab = this._selectTab.bind(this);
@@ -143,7 +151,7 @@ class Chart extends Component {
         const r = [b0, b1, b2, b3]
           .map(v => Math.round(v))
           .reduce((o, v, idx) => {
-            o['b' + idx] = v;
+            o["b" + idx] = v;
             return o;
           }, {});
         const d = { ...o, ...r };
@@ -152,16 +160,16 @@ class Chart extends Component {
       });
 
     return h(
-      'div',
+      "div",
       { style: styles.wrapper },
       h(
-        'div',
+        "div",
         { style: styles.chartTitle },
-        h('h1', {}, currTab),
-        h('div', {}, `(${hints[currTab]})`)
+        h("h1", {}, currTab),
+        h("div", {}, `(${hints[currTab]})`)
       ),
       h(Tabs, {
-        tabs: ['List', 'Array', 'Map', 'Set'],
+        tabs: ["List", "Array", "Map", "Set"],
         onSelect: this._selectTab,
         currTab
       }),
@@ -171,54 +179,54 @@ class Chart extends Component {
         h(
           BarChart,
           { data },
-          h(XAxis, { dataKey: 'name' }),
+          h(XAxis, { dataKey: "name" }),
           h(YAxis, { tickFormatter: formatSize, padding: { top: 16 } }),
-          h(CartesianGrid, { strokeDasharray: '1 1' }),
+          h(CartesianGrid, { strokeDasharray: "1 1" }),
           h(Tooltip, { content: renderToolTip, animationDuration: 200 }),
           h(Legend, {
             content: renderLegend,
-            align: 'right',
-            verticalAlign: 'middle',
-            layout: 'vertical',
+            align: "right",
+            verticalAlign: "middle",
+            layout: "vertical",
             disabled: hiddenKeys,
             onClick: ({ dataKey }) => this._toggleDataKey(hiddenKeys, dataKey)
           }),
           h(Bar, {
-            dataKey: 'b0',
-            stackId: 's',
+            dataKey: "b0",
+            stackId: "s",
             fill: colors.b0,
-            shape: shapeBar('b0', currTab)
+            shape: shapeBar("b0", currTab)
           }),
           h(Bar, {
-            dataKey: 'b1',
-            stackId: 's',
+            dataKey: "b1",
+            stackId: "s",
             fill: colors.b1,
-            shape: shapeBar('b1', currTab)
+            shape: shapeBar("b1", currTab)
           }),
           h(Bar, {
-            dataKey: 'b2',
-            stackId: 's',
+            dataKey: "b2",
+            stackId: "s",
             fill: colors.b2,
-            shape: shapeBar('b2', currTab)
+            shape: shapeBar("b2", currTab)
           }),
           h(Bar, {
-            dataKey: 'b3',
-            stackId: 's',
+            dataKey: "b3",
+            stackId: "s",
             fill: colors.b3,
-            shape: shapeBar('b3', currTab)
+            shape: shapeBar("b3", currTab)
           })
         )
       ),
       h(
-        'div',
+        "div",
         {},
         h(
-          'a',
+          "a",
           {
-            href: 'https://github.com/roman01la/js-memory-usage',
+            href: "https://github.com/roman01la/js-memory-usage",
             style: styles.link
           },
-          'GitHub'
+          "GitHub"
         )
       )
     );
